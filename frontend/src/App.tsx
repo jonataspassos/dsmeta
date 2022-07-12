4c093deb9af1b059dcc07fd49af667fc448e4b4e
Jônatas Passos <jonatascastropassos@gmail.com>
2022-07-12 12:09:21 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,14 @@
+import NotificationButton from "./components/NotificationButton/intex"
+
 function App() {
   return (
+    <>
     <h1>Olá Mundo!</h1>
+    <NotificationButton></NotificationButton>
+    <NotificationButton></NotificationButton>
+    <NotificationButton></NotificationButton>
+    <NotificationButton></NotificationButton>
+    </>
   )
 }
 
