5507452640ee7c25947685947e0041f2144d338e
Jônatas Passos <jonatascastropassos@gmail.com>
2022-07-12 13:16:07 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton/intex"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-    <h1>Olá Mundo!</h1>
-    <NotificationButton></NotificationButton>
-    <NotificationButton></NotificationButton>
-    <NotificationButton></NotificationButton>
-    <NotificationButton></NotificationButton>
+      <Header/>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
